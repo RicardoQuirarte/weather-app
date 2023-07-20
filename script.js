c760b36f17697b0e013ae31b74c6e9868c19a4c1
Reynaldo Ricardo Quirarte Huerta <ricardoquirarte@MacBook-Air-de-Reynaldo.local>
2023-07-19 18:15:48 -0600

    all working properly

--- script.js
+++ script.js
@@ -20,9 +20,15 @@ async function locationWeather(userValue) {
         const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=3fdde7a9b15741bc90763618231107&q=${userValue}`, {mode: 'cors'});
         const locationData = await response.json();
         condition.textContent = locationData.current.condition.text;
-        weather.textContent = locationData.current.temp_c + ' °C';
+        weather.textContent = Math.round(locationData.current.temp_c) + ' °C';
+        if(!toggle) {
+            weather.textContent = Math.round(locationData.current.temp_f) + ' °F';
+        }
         city.textContent = locationData.location.name;
-        feeling.textContent = locationData.current.feelslike_c + ' °C';
+        feeling.textContent = Math.round(locationData.current.feelslike_c) + ' °C';
+        if(!toggle) {
+            feeling.textContent = Math.round(locationData.current.feelslike_f) + ' °F';
+        }
         humidity.textContent = locationData.current.humidity + ' %';
         wind.textContent = locationData.current.wind_kph + ' km/h';
         if(locationData.current.temp_c > 30) {
@@ -35,22 +41,6 @@ async function locationWeather(userValue) {
             conditionImg.src = './mid-icon.svg';
             document.body.style.backgroundImage = "url('./mid.jpg')";
         }
-        toggleBtn.addEventListener('change', () => {
-            if(toggle) {
-                weather.textContent = locationData.current.temp_f + ' °F';
-                feeling.textContent = locationData.current.feelslike_f + ' °F';
-                celsius.style.color = 'black';
-                fahrenheit.style.color = 'white';
-                toggle = false;
-            } else {
-                weather.textContent = locationData.current.temp_c + ' °C';
-                feeling.textContent = locationData.current.feelslike_c + ' °C';
-                celsius.style.color = 'white';
-                fahrenheit.style.color = 'black';
-                toggle = true;
-            }
-            console.log('hello');
-        });
         hideLoading();
     }
     catch {
@@ -58,6 +48,23 @@ async function locationWeather(userValue) {
         hideLoading();
     }
 }
+
+toggleBtn.addEventListener('change', () => {
+    if(toggle) {
+        weather.textContent = Math.round(parseInt(weather.textContent) * 1.8 + 32) + ' °F';
+        feeling.textContent = Math.round(parseInt(feeling.textContent) * 1.8 + 32) + ' °F';
+        celsius.style.color = 'black';
+        fahrenheit.style.color = 'white';
+        toggle = false;
+    } else {
+        weather.textContent = Math.round((parseInt(weather.textContent) - 32) / 1.8) + ' °C';
+        feeling.textContent = Math.round((parseInt(feeling.textContent) - 32) / 1.8) + ' °C';
+        celsius.style.color = 'white';
+        fahrenheit.style.color = 'black';
+        toggle = true;
+    }
+    console.log('hello');
+});
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
